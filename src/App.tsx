61ef71bde446bad7428f0907ba3a764b171702fb
microQZR <zi.han.meng100@gmail.com>
2021-08-14 15:35:42 -0400

    Added additional content loading

--- src/App.tsx
+++ src/App.tsx
@@ -12,11 +12,20 @@ export type MovieJsonEntry = {
   overview: string;
 };
 
+type RawQueryResult = {
+  page: number;
+  results: MovieJsonEntry[];
+  total_pages: number;
+  total_results: number;
+};
+
 const App: FC = () => {
   const [queryResult, setQueryResult] = useState<MovieJsonEntry[]>();
   const [searchTerm, setSearchTerm] = useState("");
   const [madeNoSearch, setMadeNoSearch] = useState(true);
   const [isLoading, setIsLoading] = useState(false);
+  const [hasNoMoreResults, setHasNoMoreResults] = useState(true);
+  const [rawQueryResult, setRawQueryResult] = useState<RawQueryResult>();
   const timeoutId = useRef<number>();
 
   async function fetchMovies(value?: string) {
@@ -35,11 +44,13 @@ const App: FC = () => {
       );
       const data = await response.json();
 
+      setRawQueryResult(data);
       setQueryResult(data.results);
     } catch (err) {
       console.log(err);
     }
 
+    setHasNoMoreResults(false);
     setIsLoading(false);
   }
 
@@ -56,11 +67,43 @@ const App: FC = () => {
     timeoutId.current = window.setTimeout(() => fetchMovies(e.target.value), 200);
   }
 
+  async function loadMoreHandler() {
+    if (rawQueryResult!.page >= rawQueryResult!.total_pages) {
+      setHasNoMoreResults(true);
+      return;
+    }
+
+    setIsLoading(true);
+
+    try {
+      const response = await fetch(
+        `https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_TMDB_API_KEY}&language=en-US&page=${
+          rawQueryResult!.page + 1
+        }&include_adult=false&query=${searchTerm}`
+      );
+      const data = await response.json();
+
+      setRawQueryResult(data);
+      setQueryResult(queryResult?.concat(data.results));
+    } catch (err) {
+      console.log(err);
+    }
+
+    setHasNoMoreResults(false);
+    setIsLoading(false);
+  }
+
   return (
     <>
       <FixedTopSearchBar searchMovie={searchMovie} searchTerm={searchTerm} onInputChange={searchBarInputOnChangeHandler} />
       <div className={`flex-column space-between ${classes.contentContainer}`}>
-        <SearchResultList madeNoSearch={madeNoSearch} queryResult={queryResult} isLoading={isLoading} />
+        <SearchResultList
+          madeNoSearch={madeNoSearch}
+          queryResult={queryResult}
+          isLoading={isLoading}
+          hasNoMoreResults={hasNoMoreResults}
+          loadMoreHandler={loadMoreHandler}
+        />
         <Footer />
       </div>
     </>
--- src/components/SearchResultList.tsx
+++ src/components/SearchResultList.tsx
@@ -1,26 +1,52 @@
-import { FC } from "react";
+import { FC, useRef } from "react";
 import { MovieJsonEntry } from "../App";
 import SearchResultItem from "./SearchResultItem";
 import classes from "./SearchResultList.module.css";
 import spinner from "../graphic/Dual Ring-1s-200px.svg";
 
-const SearchResultList: FC<{ madeNoSearch: boolean; queryResult: MovieJsonEntry[] | undefined; isLoading: boolean }> = props => {
+const SearchResultList: FC<{
+  madeNoSearch: boolean;
+  queryResult: MovieJsonEntry[] | undefined;
+  isLoading: boolean;
+  hasNoMoreResults: boolean;
+  loadMoreHandler: () => void;
+}> = props => {
+  const loadMoreButtonRef = useRef<HTMLButtonElement>(null);
   const queryResultMovieEntries = props.queryResult ? props.queryResult : [];
 
-  return props.isLoading ? (
-    <div className={`container flex ${classes.spinnerContainer}`}>
-      <img src={spinner} alt="loading content" className={classes.spinner} />
-    </div>
-  ) : props.madeNoSearch ? (
-    <div className={`container ${classes.searchResultsPlaceholder}`}>Search now to find flicks that you'll love.</div>
-  ) : queryResultMovieEntries.length === 0 ? (
-    <div className={`container ${classes.searchResultsPlaceholder}`}>No result has been found.</div>
-  ) : (
-    <div className={`container ${classes.searchResults}`}>
-      {queryResultMovieEntries.map(movieEntry => (
-        <SearchResultItem key={movieEntry.id} movieEntry={movieEntry} />
-      ))}
-    </div>
+  return (
+    <>
+      {props.isLoading ? (
+        <div className={`container flex ${classes.spinnerContainer}`}>
+          <img src={spinner} alt="loading content" className={classes.spinner} />
+        </div>
+      ) : props.madeNoSearch ? (
+        <div className={`container ${classes.searchResultsPlaceholder}`}>Search now to find flicks that you'll love.</div>
+      ) : queryResultMovieEntries.length === 0 ? (
+        <div className={`container ${classes.searchResultsPlaceholder}`}>No result has been found.</div>
+      ) : (
+        <div className={`container ${classes.searchResults}`}>
+          {queryResultMovieEntries.map(movieEntry => (
+            <SearchResultItem key={movieEntry.id} movieEntry={movieEntry} />
+          ))}
+        </div>
+      )}
+      {queryResultMovieEntries.length > 0 && !props.madeNoSearch && !props.hasNoMoreResults && (
+        <button
+          className={classes.loadMore}
+          ref={loadMoreButtonRef}
+          onClick={e => {
+            props.loadMoreHandler();
+            setTimeout(() => loadMoreButtonRef.current?.scrollIntoView(false), 200);
+          }}
+        >
+          Load More Results
+        </button>
+      )}
+      {queryResultMovieEntries.length > 0 && !props.madeNoSearch && props.hasNoMoreResults && (
+        <div className={classes.endOfResults}>End of Results</div>
+      )}
+    </>
   );
 };
 
